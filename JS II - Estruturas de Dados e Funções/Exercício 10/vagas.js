67ca61224038de239ad5ce9fdabf9b0f90ebc3ce
Gustavo <gustavodecristi@outlook.com>
2024-03-27 17:58:59 -0300

    Iniciando ex 10

---
+++ JS II - Estruturas de Dados e Funções/Exercício 10/vagas.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+
+
+
+do {
+    opcao = prompt(
+        "Sistema de Vagas\n" +
+        "1. Listar vagas disponíveis\n" +
+        "2. Criar um nova vaga\n" +
+        "3. Visualizar uma vaga\n" +
+        "4. Inscrever candidato\n" +
+        "5. Excluir uma vaga\n" +
+        "6. Sair\n"
+    )
+
+    switch (opcao) {
+        case "1":
+
+            break
+        case "2":
+
+            break
+        case "3":
+
+            break
+        case "4":
+
+            break
+        case "5":
+
+            break
+        case "6":
+            alert("Saindo...")
+            break
+        default:
+            alert("Opção inválida!")
+            break
+    }
+
+} while (opcao != "6");
